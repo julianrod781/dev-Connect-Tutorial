a06465654bdf755e74de6f8d0f301ce60d9b7c06
Julian Rodriguez <julianrod781@hotmail.com>
2020-05-27 18:55:22 -0500

    Added font awesome, started making homepage, register, and sign in components, created redux stores for alerts and authentication. Currently working on sign in actions in redux

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,34 +1,36 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 // used to get gravatar
-const gravatar = require('gravatar');
+const gravatar = require("gravatar");
 // used to encrypt password
-const bcrypt = require('bcryptjs');
+const bcrypt = require("bcryptjs");
 // Json web token for authentication
-const jwt = require('jsonwebtoken');
-const config = require('config');
+const jwt = require("jsonwebtoken");
+const config = require("config");
 
 //used to validate correct fields in req.body
-const { check, validationResult } = require('express-validator');
+const { check, validationResult } = require("express-validator");
 
 // Bring in User model
-const User = require('../../models/User');
+const User = require("../../models/User");
 
 //@route POST api/users
 //@desc Register user
 //@acess Public
 router.post(
-  '/',
+  "/",
   [
-    check('name', 'Name us required').not().isEmpty(),
-    check('email', 'Please include a valid email').isEmail(),
+    check("name", "Name us required").not().isEmpty(),
+    check("email", "Please include a valid email").isEmail(),
     check(
-      'password',
-      'Please enter a password with 6 or more characters'
+      "password",
+      "Please enter a password with 6 or more characters"
     ).isLength({ min: 6 }),
   ],
   async (req, res) => {
     const errors = validationResult(req);
+    console.log(req.body);
+
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
@@ -41,13 +43,13 @@ router.post(
       if (user) {
         return res
           .status(400)
-          .json({ errors: [{ msg: 'User already exist' }] });
+          .json({ errors: [{ msg: "User already exist" }] });
       }
       // Get users gravatar
       const avatar = gravatar.url(email, {
-        s: '200',
-        r: 'pg',
-        d: 'mm',
+        s: "200",
+        r: "pg",
+        d: "mm",
       });
 
       user = new User({
@@ -71,7 +73,7 @@ router.post(
 
       jwt.sign(
         payload,
-        config.get('jwtSecret'),
+        config.get("jwtSecret"),
         { expiresIn: 360000 },
         (err, token) => {
           if (err) throw err;
@@ -80,7 +82,7 @@ router.post(
       );
     } catch (error) {
       console.log(error.message);
-      res.status(500).send('Server Error');
+      res.status(500).send("Server Error");
     }
   }
 );
