076a34f566e1fd3c3ffc9b9f936d1ec8e3961ccd
Julianrod <julianrod781@hotmail.com>
2020-06-01 14:40:25 -0500

    Prepared for deployment

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const path = require('path');
 
 const app = express();
 
@@ -9,8 +10,6 @@ connectDB();
 //Init Middleware
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) => res.send('API Running'));
-
 // Define routes
 app.use('/api/users', require('./routes/api/users'));
 
@@ -19,6 +18,16 @@ app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 
 app.use('/api/posts', require('./routes/api/posts'));
+
+//Serve static assets in production
+if (process.env.Node_ENV == 'production') {
+  //Set Static Folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
